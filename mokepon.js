04ee5b482ad7f9a6d5ae5dc14ba7a021ead5ab08
Jose Rios <jrbernabe@gmail.com>
2022-07-29 13:16:53 -0400

    Order variables to avoid repeat code

--- mokepon.js
+++ mokepon.js
@@ -1,53 +1,57 @@
+const sectionSelectPet = document.getElementById('select-pet');
+const sectionSelectAttack = document.getElementById('select-attack');
+const sectionMessages = document.getElementById('messages');
+const sectionBattles = document.getElementById('battles');
+
+const restart = document.getElementById('restart');
+
+const buttonPlayerPet = document.getElementById('button-pet');
+const buttonFire = document.getElementById('button-fire');
+const buttonWater = document.getElementById('button-water');
+const buttonPlant = document.getElementById('button-plant');
+const buttonRestart = document.getElementById('button-restart');
+
+const inputHipodoge = document.getElementById('hipodoge');
+const inputCapipepo = document.getElementById('capipepo');
+const inputRatigueya = document.getElementById('ratigueya');
+const namePlayerPet = document.getElementById('pet-player');
+const nameEnemyPet = document.getElementById('pet-enemy');
+const result = document.getElementById('result');
+const resultPlayerAttack = document.getElementById('attack-player');
+const resultEnemyAttack = document.getElementById('attack-enemy');
+const textPlayerLives = document.getElementById('lives-player');
+const textEnemyLives = document.getElementById('lives-enemy');
+
 let playerAttack;
 let enemyAttack;
 let playerLives = 3;
 let enemyLives = 3;
 
 function startGame() {
-    const sectionSelectAttack = document.getElementById('select-attack');
-    const sectionRestart = document.getElementById('restart');
-    const sectionMessages = document.getElementById('messages');
-    const sectionBattles = document.getElementById('battles');
     sectionSelectAttack.style.display = 'none';
-    sectionRestart.style.display = 'none';
+    restart.style.display = 'none';
     sectionMessages.style.display = 'none';
     sectionBattles.style.display = 'none';
     
-    const buttonPlayerPet = document.getElementById('button-pet');
     buttonPlayerPet.addEventListener('click', selectPlayerPet);
-    
-    const buttonFire = document.getElementById('button-fire');
-    const buttonWater = document.getElementById('button-water');
-    const buttonPlant = document.getElementById('button-plant');
     buttonFire.addEventListener('click', attackFire);
     buttonWater.addEventListener('click', attackWater);
     buttonPlant.addEventListener('click', attackPlant);
-    
-    const buttonRestart = document.getElementById('button-restart');
     buttonRestart.addEventListener('click', restartGame);
 }
 
 function selectPlayerPet() {
-    const sectionSelectAttack = document.getElementById('select-attack');
-    const sectionSelectPet = document.getElementById('select-pet');
-    const sectionMessages = document.getElementById('messages');
-    const sectionBattles = document.getElementById('battles')
     sectionSelectAttack.style.display = 'flex';
     sectionSelectPet.style.display = 'none';
     sectionMessages.style.display = 'flex';
     sectionBattles.style.display = 'grid';
     
-    const inputHipodoge = document.getElementById('hipodoge');
-    const inputCapipepo = document.getElementById('capipepo');
-    const inputRatigueya = document.getElementById('ratigueya');
-    const spanPlayerPet = document.getElementById('pet-player');
-    
     if (inputHipodoge.checked) {
-        spanPlayerPet.innerHTML = 'Hipodoge';
+        namePlayerPet.innerHTML = 'Hipodoge';
     } else if (inputCapipepo.checked) {
-        spanPlayerPet.innerHTML = 'Capipepo';
+        namePlayerPet.innerHTML = 'Capipepo';
     } else if (inputRatigueya.checked) {
-        spanPlayerPet.innerHTML = 'Ratigueya';
+        namePlayerPet.innerHTML = 'Ratigueya';
     } else {
         alert('Select a pet');
         sectionSelectAttack.style.display = 'none';
@@ -60,14 +64,13 @@ function selectPlayerPet() {
 }
 
 function selectEnemyPet () {
-    const spanEnemyPet = document.getElementById('pet-enemy');
     let randPet = randNum(1, 3);
     if (randPet == 1) {
-        spanEnemyPet.innerHTML = 'Hipodoge';
+        nameEnemyPet.innerHTML = 'Hipodoge';
     } else if (randPet == 2) {
-        spanEnemyPet.innerHTML = 'Capipepo';
+        nameEnemyPet.innerHTML = 'Capipepo';
     } else if (randPet == 3) {
-        spanEnemyPet.innerHTML = 'Ratigueya';
+        nameEnemyPet.innerHTML = 'Ratigueya';
     }
 }
 
@@ -98,56 +101,33 @@ function randEnemyAttack() {
 }
 
 function createMessage(battleResult) {
-    const result = document.getElementById('result');
-    const resultPlayerAttack = document.getElementById('attack-player');
-    const resultEnemyAttack = document.getElementById('attack-enemy');
-    
     result.innerHTML = battleResult ;
     resultPlayerAttack.innerHTML = enemyAttack;
     resultEnemyAttack.innerHTML = playerAttack;
-    // const messages = document.getElementById('messages');
-    // let parrafo = document.createElement('p');
-    // parrafo.innerHTML = `Your pet used a ${playerAttack} attack, the enemy's pet used a ${enemyAttack} attack - ${battleResult}`;
-    // messages.appendChild(parrafo);
 }
 
 function createFinalMessage(battleResult) {
-    const sectionRestart = document.getElementById('restart');
-    sectionRestart.style.display = 'block';
-    const result = document.getElementById('result');
+    restart.style.display = 'block';
     result.innerHTML = battleResult;
-    // const messages = document.getElementById('messages');
-    // let parrafo = document.createElement('p');
-    // parrafo.innerHTML = battleResult;
-    // messages.appendChild(parrafo);
-    
-    const buttonFire = document.getElementById('button-fire');
-    const buttonWater = document.getElementById('button-water');
-    const buttonPlant = document.getElementById('button-plant');
     buttonFire.disabled = true;
     buttonWater.disabled = true;
     buttonPlant.disabled = true;
 }
 
 function battle() {
-    
-
-    const spanPlayerLives = document.getElementById('lives-player');
-    const spanEnemyLives = document.getElementById('lives-enemy');
     if (playerAttack === enemyAttack) {
         createMessage(`It's a draw 😐`);
     } else if ( (playerAttack === 'FIRE' && enemyAttack === 'PLANT') || (playerAttack === 'WATER' && enemyAttack === 'FIRE') || (playerAttack === 'PLANT' && enemyAttack === 'WATER')) {
         enemyLives--;
-        spanEnemyLives.innerHTML = enemyLives;
+        textEnemyLives.innerHTML = enemyLives;
         createMessage('You win 🥳');
     } else {
         playerLives--;
-        spanPlayerLives.innerHTML = playerLives;
+        textPlayerLives.innerHTML = playerLives;
         createMessage('You Lose 😢');
     }
 
     reviewLives();    
-    
 }
 
 function reviewLives() {
